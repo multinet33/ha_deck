5d2a80a0a55b712aa0374fd17a2cfb67f9fc66f4
Nicolas <nicolas.rebillard@laposte.net>
2025-02-17 21:15:40 +0100

    Update hd_device_sc01_plus.cpp

--- components/hd_device_sc01_plus/hd_device_sc01_plus.cpp
+++ components/hd_device_sc01_plus/hd_device_sc01_plus.cpp
@@ -53,7 +53,7 @@ void HaDeckDevice::setup() {
     disp_drv.hor_res = TFT_WIDTH;
     disp_drv.ver_res = TFT_HEIGHT;
     disp_drv.rotated = 2;
-    disp_drv.sw_rotate = 0;
+    disp_drv.sw_rotate = 1;
     disp_drv.flush_cb = flush_pixels;
     disp_drv.draw_buf = &draw_buf;
     lv_disp_t *disp = lv_disp_drv_register(&disp_drv);
